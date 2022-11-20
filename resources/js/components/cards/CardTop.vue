5bd76c193cad9efc9060c53791b7b5e8f6aad523
Monique Raats-Bartelings <monique@orthobegeleiding.nl>
2022-11-20 21:05:38 +0100

    cardTop

---
+++ resources/js/components/cards/CardTop.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="card-top">
+      <slot></slot>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    name: 'CardTop'
+  }
+  </script>
+
+  <style lang="scss" scoped>
+  @import "../../../sass/mixins.scss";
+  @import "../../../sass/variables";
+    .card-top {
+      position: relative;
+      background: rgba($color-black, .15);
+      padding: 1.25rem;
+
+      &:before {
+          content: '';
+          position: absolute;
+          top: -2px;
+          left: 0;
+          height: 3px;
+          width: 100%;
+          background-color: var(--v-primary-base);
+      }
+
+      &.half {
+        width: 50vw;
+        @include respond(md) {
+            width: 70vw;
+        }
+
+        @include respond(sm) {
+            width: 80vw;
+        }
+
+        @include respond(xs) {
+            width: 100%;
+        }
+      }
+    }
+  </style>
---
+++ resources/js/vuetify.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
+
+export default new Vuetify({
+    theme: {
+        options: { customProperties: true },
+        themes: {
+            light: {
+                primary: '#8672C0',
+                //secondary: '#e78856',
+                secondary: '#E9B049', // sunray yellow
+                gold: '#C69963', // gold
+                //secondary: '#533f8d', // donker paars
+                accent: '#533f8d',
+
+                info: '#4856ad',
+                //warning: '#D06A4C', // Copper Red
+                warning: '#e78856', // Orthobegeleiding orange
+                //warning: '#e9b049', // sunray yellow
+                error: '#b6244f',
+                success: '#c6c86a',
+
+                light1: '#f1eee4',
+                light2: '#E4D8C6',
+
+                grey1: '#999999',
+                grey2: '#333333',
+              }
+        },
+    },
+})
