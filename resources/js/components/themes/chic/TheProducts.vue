cc306dfcf8e41779e7b9e9fc0c624a87571e7f6e
Monique Raats-Bartelings <monique@orthobegeleiding.nl>
2023-02-22 17:02:02 +0100

    products 1

--- resources/js/components/themes/chic/TheProducts.vue
+++ resources/js/components/themes/chic/TheProducts.vue
@@ -4,23 +4,36 @@
             v-for="product in products"
             :key="product.name"
         >
-        {{ product.title }}
+            <img :src="product.src" :alt="product.alt" class="product__img">
+            <h5 class="product__name">{{ product.title }}</h5>
+            <div class="product__attribute" v-for="(attr, index) in product.features" :key="index">
+                <v-icon class="product__icon" color="primary" size="2rem">{{ attr.icon }}</v-icon>
+                <p>{{ attr.text }}</p>
+            </div>
+            <button class="chic__btn chic__btn--filled">Order</button>
         </div>
     </section>
 </template>
 
 <script>
+import balloon1 from "../../../../images/one-balloon.jpg";
+import balloon2 from "../../../../images/two-balloons.jpg";
+import balloon3 from "../../../../images/balloons4.jpg";
+import diagram from "../../../../images/hand-met-diagram.jpg"
+import parachute from "../../../../images/parachute.jpg";
+import balloons from "../../../../images/small-balloons.jpg";
+
 export default {
     name: 'products-section',
     data() {
         return {
             products: [
-                {name: 'home-card-1', title: 'Home card 1'},
-                {name: 'home-card-2', title: 'Home card 2'},
-                {name: 'home-card-3', title: 'Home card 3'},
-                {name: 'home-card-4', title: 'Home card 4'},
-                {name: 'home-card-5', title: 'Home card 5'},
-                {name: 'home-card-6', title: 'Home card 6'}
+                {name: 'product-1', title: 'Product 1', src: balloon1, alt: 'One balloon', features: [{icon: 'mdi-web', text: 1, label: 'Website'}, {icon: 'mdi-material-design', text: 1, label: 'Standard design'}, {icon: 'mdi-pencil-ruler', text: 0, label: 'Own design'}, {icon: 'mdi-currency-eur', text: 400, label: 'Price'}]},
+                {name: 'product-2', title: 'Product 2', src: balloon2, alt: 'Two balloons', features: [{icon: 'mdi-web', text: 2, label: 'Website'}, {icon: 'mdi-material-design', text: 1, label: 'Standard design'}, {icon: 'mdi-pencil-ruler', text: 0, label: 'Own design'}, {icon: 'mdi-currency-eur', text: 700, label: 'Price'}]},
+                {name: 'product-3', title: 'Product 3', src: balloon3, alt: 'Three balloons', features: [{icon: 'mdi-web', text: '3-10', label: 'Website'}, {icon: 'mdi-material-design', text: 1, label: 'Standard design'}, {icon: 'mdi-pencil-ruler', text: 0, label: 'Own design'}, {icon: 'mdi-currency-eur', text: 1000, label: 'Price'}]},
+                {name: 'product-4', title: 'Product 4', src: diagram, alt: 'Diagram', features: [{icon: 'mdi-web', text: 1, label: 'Website'}, {icon: 'mdi-material-design', text: 0, label: 'Standard design'}, {icon: 'mdi-pencil-ruler', text: 1, label: 'Own design'}, {icon: 'mdi-currency-eur', text: 800, label: 'Price'}]},
+                {name: 'product-5', title: 'Product 5', src: parachute, alt: 'Parachute', features: [{icon: 'mdi-web', text: 2, label: 'Website'}, {icon: 'mdi-material-design', text: 0, label: 'Standard design'}, {icon: 'mdi-pencil-ruler', text: 1, label: 'Own design'}, {icon: 'mdi-currency-eur', text: 1100, label: 'Price'}]},
+                {name: 'product-6', title: 'Product 6', src: balloons, alt: 'Balloons', features: [{icon: 'mdi-web', text: '3-10', label: 'Website'}, {icon: 'mdi-material-design', text: 0, label: 'Standard design'}, {icon: 'mdi-pencil-ruler', text: 1, label: 'Own design'}, {icon: 'mdi-currency-eur', text: 1500, label: 'Price'}]}
             ]
         }
     }
@@ -37,6 +50,25 @@ export default {
     }
     .product {
         background-color: var(--v-light1-base);
+
+        &__img {
+            width: 100%;
+        }
+
+        &__name {
+
+        }
+
+        &__attribute {
+
+            p {
+
+            }
+        }
+
+        &__icon {
+
+        }
     }
 
 </style>
